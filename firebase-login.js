987e28a82922504d2582f78b32d754d85d5b94ca
a9dp <59792641+a9dp@users.noreply.github.com>
2020-01-15 03:56:11 +0700

    Update firebase-login.js

--- firebase-login.js
+++ firebase-login.js
@@ -1,7 +1,7 @@
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-alert(JSON.stringify(firebase.auth.currentUser)) 
+alert(JSON.stringify(firebase.auth().currentUser )) 
 	my('#user-pic').css("background-image", "url(" + user.photoURL + ")")
 	
 	my('#user-name').text(user.displayName).unAttr("hidden")
