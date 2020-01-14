02e99a05b674c9316048403f4d8e6b8510eb9f50
a9dp <59792641+a9dp@users.noreply.github.com>
2020-01-14 18:12:52 +0700

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -215,34 +215,3 @@ FriendlyChat.prototype.checkSetup = function() {
 
 
   window.friendlyChat = new FriendlyChat();
-
-addEventListener("load", function () {
-
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-	my('#user-pic').css("background-image", "url(" + user.photoURL + ")")
-	
-	my('#user-name').text(user.displayName).unAttr("hidden")
-	
-	my('#sign-out').unAttr('hidden')
-	
-	my('#sign-in').attr('hidden', true)
-	
-  } else {
-  	my('#user-pic').css("background-image",  'url("/images/no-login.png")')
-	
-	my('#user-name').attr('hidden', true)
-	
-	my('#sign-out').attr('hidden', true)
-	
-	my('#sign-in').unAttr('hidden')
-  }
-})
-
-my('#sign-in').click(function () {
-    friendlyChat.signInFb()
-})
-my('#sign-out').click(function () {
-    friendlyChat.signOut()
-})
-})
