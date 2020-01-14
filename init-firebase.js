b5fc5d4f1cb0fb543400831c0022fee7b2af3331
a9dp <59792641+a9dp@users.noreply.github.com>
2020-01-14 13:19:07 +0700

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,218 @@
+var config = {
+	apiKey: "AIzaSyATsprQmBaksAg7ZPoGGejFTLWclLP576Y",
+	authDomain: "glass-gasket-254006.firebaseapp.com",
+	databaseURL: "https://glass-gasket-254006.firebaseio.com",
+	projectId: "glass-gasket-254006",
+	storageBucket: "glass-gasket-254006.appspot.com",
+	messagingSenderId: "883400548883",
+	appId: "1:883400548883:web:61d2e40bfa097c9ba4d5e6",
+	measurementId: "G-GZ498J99QG"
+};
+  // Initialize Firebase
+  firebase.initializeApp(config);
+
+
+function FriendlyChat() {
+  this.checkSetup();
+  this.initFirebase();
+}
+
+
+FriendlyChat.prototype.initFirebase = function() {
+
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  this.auth.onAuthStateChanged(
+	this.onAuthStateChanged
+	.bind(this)
+  )
+}
+
+FriendlyChat.prototype.loadMessages = function() {
+
+  this.messagesRef = this.database.ref('messages');
+
+  this.messagesRef.off();
+  this.messagesRef.limitToLast(12).on('child_added', setMessage);
+  this.messagesRef.limitToLast(12).on('child_changed', setMessage);
+};
+
+// Saves a new message on the Firebase DB.
+FriendlyChat.prototype.saveMessage = function(posted) {
+
+  if (posted && this.checkSignedInWithMessage()) {
+
+	var currentUser = this.auth.currentUser
+
+	this.messagesRef.push({
+	  name: currentUser.displayName,
+	  text: posted.text,
+	  title: posted.title,
+	  posted: posted.posted,
+	  time: Date.now(),
+	  photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
+	}).then(function() {
+		//
+	})
+	.catch(function(error) {
+	  console.error('Error writing to Firebase Database', error)
+	})
+  }
+}
+
+
+FriendlyChat.prototype.saveImagePoseted = function (file) {
+  
+  var myObject = my({})
+  if (this.checkSignedInWithMessage()) {
+
+	var e = this.storage
+	.ref('posted-image')
+	.put(file, {
+		contentType: file.type
+	})
+	
+	e.on("state_changed",
+	function () {
+		myObject.trigger('progess')
+	}, function () {
+		myObject.trigger('catch')
+	}, function () {
+		e.snapshot.ref
+		.getDownloadURL()
+		.then(function (url) {
+			myObject.trigger('then', url)
+		})
+	})
+
+  }
+  else myObject.trigger('catch');
+  
+  return {
+	  then: function (fn) {
+		  myObject.on('then')
+		  .then(function (e) {
+			  fn.call(this, e.detail)
+		  })
+	  },
+	  catch: function (fn) {
+		  myObject.on('catch')
+		  .then(function (e) {
+			  fn.call(this, e.detail)
+		  })
+	  }
+  }
+  
+}
+
+
+FriendlyChat.prototype.signIn = function() {
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider)
+}
+
+FriendlyChat.prototype.signInFb = function() {
+
+	var provider = new firebase.auth
+	.FacebookAuthProvider()
+	
+	provider
+	.addScope('user_birthday')
+	
+	provider.setCustomParameters({
+		'display': 'popup'
+	})
+	
+	firebase.auth()
+	.signInWithRedirect(provider);
+	
+	firebase.auth()
+	.signInWithPopup(provider)
+	.then(function (result) {
+		var token = result
+		.credential.accessToken
+		
+	  	var user = result.user
+	}).catch(function (error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		var email = error.email;
+		var credential = error.credential
+	})
+	firebase.auth()
+	.getRedirectResult()
+	.then(function (result) {
+
+		if (result.credential) {
+			var token = resul
+				t.credential
+				.accessToken
+
+		}
+		var user = result.user;
+	}).catch(function (error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential
+	})
+}
+
+// Signs-out of Friendly Chat.
+FriendlyChat.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
+
+
+FriendlyChat.prototype.onAuthStateChanged = function (user) {
+  if (user) {
+  
+	var profilePicUrl = user.photoURL;
+	var nane = user.displayName;
+
+  } else {
+  
+  }
+}
+
+// Returns true if user is signed-in. Otherwise false and displays a message.
+FriendlyChat.prototype.checkSignedInWithMessage = function() {
+  if (this.auth.currentUser) {
+	return true;
+  }
+  return false;
+};
+
+
+FriendlyChat.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
+
+// Displays a Message in the UI.
+FriendlyChat.prototype.displayMessage = function(key, name, text, picUrl, imageUri) {
+  var div = document.getElementById(key);
+
+  this.messageList.scrollTop = this.messageList.scrollHeight;
+  this.messageInput.focus();
+};
+
+
+FriendlyChat.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+	window.alert('You have not configured and imported the Firebase SDK. ' +
+		'Make sure you go through the codelab setup instructions.');
+  } else if (config.storageBucket === '') {
+	window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+		'actually a Firebase bug that occurs rarely. ' +
+		'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+		'and make sure the storageBucket attribute is not empty. ' +
+		'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
+		'displayed there.');
+  }
+};
+
+window.onload = function() {
+  window.friendlyChat = new FriendlyChat();
+};
---
+++ main-poster.js
@@ -0,0 +1,172 @@
+onerror = function () {
+ alert(arguments[0])
+}
+
+my('#title, #descript, #content')
+.each(function (e) {
+	my(this).click(function (e) {
+		if (my(this).find('textarea[data-type="editer"]')[0])
+			return;
+			
+		try {
+			e.preventDefault()
+			e.stopPropagation()
+		} catch (e) {}
+		
+		var html = my(this).html()
+		
+		var _ = this
+		
+		var edit = my('<textarea>')
+		edit.data('type', 'editer')
+		
+		edit.html(html)
+		.css({
+			width: '100%'
+		})
+		.css({
+			height: my(this).innerHeight() + 'px'
+		})
+		.addClass('form-control border')
+		.blur(function () {
+			this.value = this.value.replace(/\n/g, '<br>')
+			my(_).html(this.value)
+		})
+		.focus()
+		my(this).empty()
+		.append(edit)
+		
+	})
+})
+my('#posted').click(function () {
+	var file, _ = this
+	var inpF = my('<input type=file accept="image/*,capture=camera">')
+	.appendTo('body')
+	.css({
+		display: 'none',
+		visible: 'none',
+		opacity: 0,
+		position: 'fixed',
+		top: '-100vh',
+		left: '-100vw'
+	})
+	.change(function () {
+alert(010)
+		file = this.files[0]
+		var read = new FileReader
+		
+		alert("read start")
+		read.onload = function () {
+			_.src = this.result
+		
+			_[my.expando + '-file'] = file
+		}
+		  read.readAsDataURL(file)
+	})
+	inpF[0].click()
+	inpF[0].focus()
+	
+	
+	
+})
+
+
+
+
+
+
+FriendlyChat.prototype.post = function (e) {
+	if (this.checkSignedInWithMessage()) {
+
+	var currentUser = this.auth.currentUser
+
+	this.messagesRef.push({
+	  name: currentUser.displayName,
+	  text: posted.text,
+	  title: posted.title,
+	  posted: posted.posted,
+	  time: Date.now(),
+	  descript: e,
+	  photoUrl: currentUser.photoURL || '/images/no-login.png'
+	}).then(function() {
+		my('#loading')
+		.attr('hidden', true)
+	})
+	.catch(function(error) {
+		my('#loading')
+		.attr('hidden', true)
+	  console.error('Error writing to Firebase Database', error)
+	})
+  }
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+	my('#user-pic')[0]
+	.backgroundImage = 'url(' + user.photoURL + ')'
+	
+	my('#user-name').text(user.displayName)
+	
+	my('#sign-out').unAttr('hidden')
+	
+	my('#sign-in').attr('hidden', true)
+	
+  } else {
+  	my('#user-pic')[0]
+	.backgroundImage = 'url(/var/mobile/Documents/a9/no-login.png)'
+	
+	my('#user-name').attr('hidden', true)
+	
+	my('#sign-out').attr('hidden', true)
+	
+	my('#sign-in').unAttr('hidden')
+  }
+})
+
+my('#sign-in').click(function () {
+    friendlyChat.signInFb()
+})
+my('#sign-out').click(function () {
+    friendlyChat.signOut()
+})
+
+my('#post-blog').click(function () {
+	
+	if (friendlyChat.checkSignedInWithMessage()) {
+	
+		my('#loading')
+		.unAttr('hidden')
+		
+		var file = my('#posted')[0][my.expando + '-file']
+		if (file) {
+			// if file exists
+			
+			var sto = friendlyChat.storage
+			.ref('posted-image')
+			.put(file, {
+				contentType: file.type
+			})
+			
+			sto.on('state_changed',
+				function () {
+					
+				},
+				function () {
+					friendlyChat.post()
+				},
+				function () {
+					sto.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+			 friendlyChat.post(downloadURL)
+ })
+				}
+			)
+			
+		}
+		else friendlyChat.post();
+		
+	}
+	else {
+		alert("You have login if active action this")
+	}
+	
+})
